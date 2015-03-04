54eb733547739ddc13068e0e8de6a92829bdcbdb
Sylvain Gamel <sylvain@SGinTech.fr>
2015-03-04 12:32:46 +0100

    Initial Commit

---
+++ SGiT_PoppingAnimationViewHelper/SGiT_PoppingAnimationViewHelper.h
@@ -0,0 +1,19 @@
+//
+//  SGiT_PoppingAnimationViewHelper.h
+//  SGiT_PoppingAnimationViewHelper
+//
+//  Created by Sylvain on 04/03/2015.
+//  Copyright (c) 2015 S.G. inTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SGiT_PoppingAnimationViewHelper.
+FOUNDATION_EXPORT double SGiT_PoppingAnimationViewHelperVersionNumber;
+
+//! Project version string for SGiT_PoppingAnimationViewHelper.
+FOUNDATION_EXPORT const unsigned char SGiT_PoppingAnimationViewHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SGiT_PoppingAnimationViewHelper/PublicHeader.h>
+
+
